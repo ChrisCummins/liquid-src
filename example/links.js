f8acd65ff500957f7fd0f54483977f631cdb53d0
Chris Cummins <chrisc.101@gmail.com>
2013-12-15 18:14:46 +0000

    Add example usage files

---
+++ example/links.js
@@ -0,0 +1,16 @@
+$.data(document.body, 'liquid-src', {
+
+  "iframe": {
+    "gangnam-style": "http://www.youtube.com/embed/9bZkp7q19f0",
+    "higgs-boson-blues": "http://www.youtube.com/embed/1GWsdqCYvgw"
+  },
+
+  "img": {
+    "google-logo": "https://www.google.co.uk/images/srpr/logo11w.png"
+  },
+
+  "script": {
+    "jquery-ui": "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"
+  }
+
+});
